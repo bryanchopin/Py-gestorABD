707fe4a3f4677958b9d04be41f5218c9f05fb5a1
brychxpin <bryan.oliveros17@tectijuana.edu.mx>
2022-04-11 12:21:59 -0700

    update and pullrequest

--- test.py
+++ test.py
@@ -44,11 +44,6 @@ from pynput import keyboard as kb
 
 
 
-def pulsa(tecla):
-    	print('Se ha pulsado la tecla ' + str(tecla))
-
-with kb.Listener(pulsa) as escuchador:
-	escuchador.join()
 
 
 # print("Emter a numbers")
